7f4c64072019c503ac96e085934e6b2aabf44100
Blayung <teamaster69@proton.me>
2024-08-30 23:37:59 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,15 @@
+[![](https://cdn.jsdelivr.net/npm/@intergrav/devins-badges@3/assets/cozy/available/modrinth_vector.svg)](https://modrinth.com/mod/no-peeking) ![](https://cdn.jsdelivr.net/npm/@intergrav/devins-badges@3/assets/cozy/unsupported/forge_vector.svg)
+
+# No Peeking!
+
+## What is this?
+This is a minecraft fabric mod that hides your password as you type when you're using a login command, often used on non-premium servers.
+
+## Downloads
+You can download this mod on modrinth. [https://modrinth.com/mod/no-peeking](https://modrinth.com/mod/no-peeking)
+
+## How to compile this mod? (assuming you're on linux)
+0. Make sure you have git and jdk 17 installed. If not, check these links out: [https://git-scm.com/downloads](https://git-scm.com/downloads), [https://www.oracle.com/pl/java/technologies/downloads](https://www.oracle.com/pl/java/technologies/downloads). You can also try to install these programs with your distro's package manager.
+1. Clone the repo: `git clone https://github.com/Blayung/no-peeking.git; cd no-peeking`
+2. Compile the mod: `./gradlew build`
+3. Now the mod jar file should be here: `./build/libs/no-peeking-1.0.0.jar`
---
+++ src/main/java/me/teamaster/nopeeking/TextFieldWidgetMixin.java
@@ -0,0 +1,33 @@
+package me.teamaster.nopeeking;
+
+import net.minecraft.client.gui.widget.TextFieldWidget;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.At;
+import org.objectweb.asm.Opcodes;
+
+@Mixin(TextFieldWidget.class)
+public class TextFieldWidgetMixin {
+    @Redirect(method = "renderButton(Lnet/minecraft/client/gui/DrawContext;IIF)V", at = @At(value = "FIELD", opcode = Opcodes.GETFIELD, target = "Lnet/minecraft/client/gui/widget/TextFieldWidget;text:Ljava/lang/String;"))
+    private String passwordObfuscationProxy(TextFieldWidget textFieldWidget) {
+        String text = textFieldWidget.getText();
+
+        if (text.startsWith("/l ") || text.startsWith("/login ") || text.startsWith("/register ")) {
+            StringBuilder obfuscatedText = new StringBuilder();
+            boolean firstPart = true;
+            for (String part : text.split(" ")) {
+                if (firstPart) {
+                    obfuscatedText.append(part);
+                    firstPart = false;
+                } else {
+                    obfuscatedText.append("*".repeat(part.length()));
+                }
+                obfuscatedText.append(' ');
+            }
+            return obfuscatedText.toString();
+        }
+
+        return text;
+    }
+}
